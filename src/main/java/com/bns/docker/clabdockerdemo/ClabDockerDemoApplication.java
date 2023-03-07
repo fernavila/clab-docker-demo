adfd5f252e9675b42d1162ba5874cb09dac1775f
Fernando Avila <huawei.fernando.avila@gmail.com>
2023-03-06 22:18:20 -0500

    load project

---
+++ src/main/java/com/bns/docker/clabdockerdemo/ClabDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bns.docker.clabdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClabDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClabDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bns/docker/clabdockerdemo/ClabDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bns.docker.clabdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClabDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
